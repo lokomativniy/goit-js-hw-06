e436b44136f4eda660a32e105df858f26a923445
Dmitriy <dmitriy.sergiyenko.87@gmail.com>
2021-10-26 08:40:45 +0300

    Update-task-05

--- src/js/task-05.js
+++ src/js/task-05.js
@@ -3,28 +3,28 @@
 <h1>Hello, <span id="name-output">Anonymous</span>!</h1> */
 }
 
-// const refs = {
-//    input :document.querySelector('#name-input'),
-//    output :document.querySelector('#name-output'),
-// };
-
-// function onInputChange(event) {
-
-//        if (event.currentTarget.value === '') {
-//          refs.output.textContent = 'Anonymous';
-//     }
-//     else {
-//       refs.output.textContent = event.currentTarget.value
-//     }
-// }
-// refs.input.addEventListener('input',onInputChange)
-
 const refs = {
-  input: document.querySelector('#name-input'),
-  output: document.querySelector('#name-output'),
+   input :document.querySelector('#name-input'),
+   output :document.querySelector('#name-output'),
 };
 
 function onInputChange(event) {
-  refs.output.textContent = event.currentTarget.value;
+
+       if (event.currentTarget.value === '') {
+         refs.output.textContent = 'Anonymous';
+    }
+    else {
+      refs.output.textContent = event.currentTarget.value
+    }
 }
-refs.input.addEventListener('input', onInputChange);
+refs.input.addEventListener('input',onInputChange)
+
+// const refs = {
+//   input: document.querySelector('#name-input'),
+//   output: document.querySelector('#name-output'),
+// };
+
+// function onInputChange(event) {
+//   refs.output.textContent = event.currentTarget.value;
+// }
+// refs.input.addEventListener('input', onInputChange);
